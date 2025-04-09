13988927e29da470befa6ef143a2268898abe07f
Liam <84729003+LiamBdr@users.noreply.github.com>
2025-04-10 01:24:07 +0200

    fix: Adjust JWT build event dispatch order to prevent validation failures

--- src/Entity/AccessToken.php
+++ src/Entity/AccessToken.php
@@ -4,32 +4,21 @@ declare(strict_types=1);
 
 namespace League\Bundle\OAuth2ServerBundle\Entity;
 
-use Lcobucci\JWT\Configuration;
-use Lcobucci\JWT\Signer\Key\InMemory;
-use Lcobucci\JWT\Token\Plain as PlainToken;
-use League\OAuth2\Server\CryptKeyInterface;
+use Lcobucci\JWT\Token;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use DateTimeImmutable;
 use League\Bundle\OAuth2ServerBundle\Event\BeforeJwtTokenBuildEvent;
 use League\Bundle\OAuth2ServerBundle\OAuth2Events;
-use DateTimeImmutable;
-use RuntimeException;
 
 class AccessToken implements AccessTokenEntityInterface
 {
     use AccessTokenTrait;
     use EntityTrait;
     use TokenEntityTrait;
-
-    private CryptKeyInterface $privateKey;
-
-    public function setPrivateKey(CryptKeyInterface $privateKey): void
-    {
-        $this->privateKey = $privateKey;
-    }
 
     private static ?EventDispatcherInterface $eventDispatcher = null;
 
@@ -38,38 +27,10 @@ class AccessToken implements AccessTokenEntityInterface
         self::$eventDispatcher = $eventDispatcher;
     }
 
-    private function convertToJWT(): PlainToken
+    private function convertToJWT(): Token
     {
-        if (!isset($this->privateKey)) {
-            throw new RuntimeException('Private key has not been set');
-        }
-
-        $signer = new \Lcobucci\JWT\Signer\Rsa\Sha256();
-        $lcobucciKey = InMemory::plainText(
-            $this->privateKey->getKeyContents(),
-            $this->privateKey->getPassPhrase() ?? ''
-        );
-
-        if ($this->privateKey->getKeyContents() === '') {
-            throw new RuntimeException('Private key is empty');
-        }
-
-        $configuration = Configuration::forAsymmetricSigner(
-            $signer,
-            $lcobucciKey,
-            InMemory::plainText('')
-        );
-
-        $builder = $configuration->builder();
-
-        $builder
-            ->permittedFor($this->getClient()->getIdentifier())
-            ->identifiedBy($this->getIdentifier())
-            ->issuedAt(new DateTimeImmutable())
-            ->canOnlyBeUsedAfter(new DateTimeImmutable())
-            ->expiresAt($this->getExpiryDateTime())
-            ->relatedTo($this->getSubjectIdentifier())
-            ->withClaim('scopes', $this->getScopes());
+        $this->initJwtConfiguration();
+        $builder = $this->jwtConfiguration->builder();
 
         if (self::$eventDispatcher) {
             $event = new BeforeJwtTokenBuildEvent($builder, $this);
@@ -77,11 +38,14 @@ class AccessToken implements AccessTokenEntityInterface
             $builder = $event->getBuilder();
         }
 
-        return $builder->getToken($configuration->signer(), $configuration->signingKey());
-    }
-
-    private function getSubjectIdentifier(): string
-    {
-        return $this->getUserIdentifier() ?? $this->getClient()->getIdentifier();
+        return $builder
+            ->permittedFor($this->getClient()->getIdentifier())
+            ->identifiedBy($this->getIdentifier())
+            ->issuedAt(new DateTimeImmutable())
+            ->canOnlyBeUsedAfter(new DateTimeImmutable())
+            ->expiresAt($this->getExpiryDateTime())
+            ->relatedTo($this->getSubjectIdentifier())
+            ->withClaim('scopes', $this->getScopes())
+            ->getToken($this->jwtConfiguration->signer(), $this->jwtConfiguration->signingKey());
     }
 }
